2becc37012834aab5d2764559ffed8f17b516f3e
amansawhney <amansawhney@sawhneygames.com>
2017-07-27 17:10:13 -0400

    added yelp and query string

--- yelp.rb
+++ yelp.rb
@@ -3,18 +3,19 @@ require 'nokogiri'
 require 'sentimental'
 require 'pragmatic_segmenter'
 
-def scrapeAmazon(url)
+def scrapeYelp(url, queryString)
 
   analyzer = Sentimental.new
   analyzer.load_defaults
   analyzer.threshold = 0.1
-    # Pull in the page
-    reviews = []
-    doc = Nokogiri::HTML(open(url,  "User-Agent" => "Ruby/#{RUBY_VERSION}",
-    "From" => "foo@bar.invalid",
-    "Referer" => "http://www.ruby-lang.org/"))
-    results = doc.css("div > span > div > div.a-expander-content.a-expander-partial-collapse-content")
-    results.each do |r|
+  # Pull in the page
+  reviews = []
+  doc = Nokogiri::HTML(open(url,  "User-Agent" => "Ruby/#{RUBY_VERSION}",
+  "From" => "foo@bar.invalid",
+  "Referer" => "http://www.ruby-lang.org/"))
+  results = doc.css("#super-container > div > div > div.column.column-alpha.main-section > div > div.feed > div.review-list > ul > li > div > div.review-wrapper > div.review-content > p")
+  results.each do |r|
+    if(r.text.downcase.include?(queryString.downcase))
       ps = PragmaticSegmenter::Segmenter.new(text: r.text)
       sentences = ps.segment
       scores = []
@@ -24,9 +25,10 @@ def scrapeAmazon(url)
       reviews.push(scores.inject{ |sum, el| sum + el }.to_f)
       reviews.push(r.text)
     end
-    return reviews
   end
+  return reviews
+end
 
-  File.write('./reviewData.txt', scrapeAmazon("https://www.amazon.com/Steal-This-Book-Abbie-Hoffman/dp/156858217X/ref=sr_1_1?ie=UTF8&qid=1501125005&sr=8-1&keywords=steal+this+book").to_s)
+File.write('./reviewData.txt', scrapeYelp("https://www.yelp.com/biz/peaches-hothouse-brooklyn", "Fried Chicken").to_s)
 #   File.open(f, 'w') { |file| file.write(scrapeAmazon("https://www.amazon.com/Neutrogena-Therapeutic-Original-Dandruff-Treatment/dp/B0009KN8UA/ref=sr_1_1?ie=UTF8&qid=1501120105&sr=8-1-spons&keywords=t%2Bgel%2Bshampoo&th=1")
 # ) }
